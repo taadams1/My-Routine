305a8f4b3492c4502755e8ca2f63a455f965106e
Trevor A <taadams1@dmacc.edu>
2017-07-14 22:50:34 -0500

    Final version of Driver class.

--- MyRoutineDriver.java
+++ MyRoutineDriver.java
@@ -1,3 +1,9 @@
+/**Driver for My Routine program, contains basic
+ * interactivity with options to add vertices,
+ * print all vertices, print all edges, and an
+ * option to quit the program
+ */
+
 package MyRoutine;
 
 import java.io.*;
@@ -14,27 +20,33 @@ public class MyRoutineDriver
 				+ "This program tracks the places you go and\n"
 				+ "how mant times they are visited.\n");
 		
+		//while loop runs program until the user chooses to quit
 		while (!done)
 		{
 			System.out.println("Enter a letter to choose an option:");
 			System.out.println("\'A\'dd a place you have been - \'S\'how where you have visited"
-					+"\n\'Q'uit the program.");
+					+"\n\'T'rips between places (printed) - 'Q'uit the program.");
 			//add option to sort by most visits later
 			int choice = (getChar());
 			
 			switch (choice)
 			{
-			case 'A':
+			case 'a':
 				System.out.println("Enter the name of the place visited: ");
 				place = getString();
 				myRoutine.addVertex(new Vertex(place));
 				break;
-			case 'S':
+			case 's':
 				System.out.println("Here are the places you've been:");
 				myRoutine.printRoutine();
-				System.out.println("----------------------------\n");
+				System.out.println("----------------------------");
 				break;
-			case 'Q':
+			case 't':
+				System.out.println("These are the trips you have made:");
+				myRoutine.printTrips();
+				System.out.println("----------------------------");
+				break;
+			case 'q':
 				done = true;
 				System.out.println("Thanks for using My Routine.");
 				break;
@@ -52,7 +64,7 @@ public class MyRoutineDriver
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		String s = br.readLine();
-		return s;
+		return s.toLowerCase();
 	}
 	
 	/**Method to read char, calls get string method for input
@@ -63,5 +75,4 @@ public class MyRoutineDriver
 		return s.charAt(0);
 	}
 	
-
 }
